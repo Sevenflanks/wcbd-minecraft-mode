a3b9d30ccf898565a354111f98c3c238170d7050
sevenflanks <godjdc@gmail.com>
2016-05-15 14:29:30 +0800

    fix itemBlock reg

--- src/main/java/org/wcbdmod/common/loader/WcbdModelsLoader.java
+++ src/main/java/org/wcbdmod/common/loader/WcbdModelsLoader.java
@@ -2,11 +2,15 @@ package org.wcbdmod.common.loader;
 
 import java.util.List;
 
+import org.wcbdmod.common.WcbdUtilsMod;
 import org.wcbdmod.elevator.block.ElevatorBlock;
 import org.wcbdmod.elevator.block.GenericBlock;
 
 import com.google.common.collect.Lists;
 
+import net.minecraft.block.Block;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.resources.model.ModelResourceLocation;
 import net.minecraft.item.Item;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 
@@ -20,9 +24,17 @@ public class WcbdModelsLoader {
 	public static final List<? extends GenericBlock> blocks = Lists.newArrayList(new ElevatorBlock());
 	public static final List<? extends Item> items = Lists.newArrayList();
 
+	// 將block的item型態註冊
+	public static void regItemBlock(Block block) {
+		final Item item = Item.getItemFromBlock(block);
+        Minecraft.getMinecraft().getRenderItem().getItemModelMesher().register
+                (item, 0, new ModelResourceLocation(WcbdUtilsMod.MODID + ":" + item.getUnlocalizedName().substring(5), "inventory"));
+    }
+	
 	public static void initBlock() {
 		for (GenericBlock block : blocks) {
 			GameRegistry.registerBlock(block, block.getItemClass(), block.getName());
+			regItemBlock(block);
 		}
 	};
 
